189c743b06fa29181696ad5b8c5f125803b5a802
Jay Kumbhar <jaykumbhar518@gmail.com>
2026-03-12 12:29:57 +0530

    Update about_app.py

--- pages/about_app.py
+++ pages/about_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="AI Crop Disease Detection",
+    page_title="🌱 AI Crop Disease Detection",
     layout="wide",
     initial_sidebar_state="collapsed"
 )
